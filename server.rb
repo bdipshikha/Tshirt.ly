ed815a895e37ed995abf9ff49ad3402dfff6026c
JAH <kwasijames@OtuJAH.local>
2015-05-13 18:18:26 -0400

    Quantity updating after purchase

--- server.rb
+++ server.rb
@@ -19,23 +19,22 @@ end
 
 
 get '/orders' do
-
     shirts = Shirt.joins(:orders)
     orders = Order.all
     erb :orders, locals: {orders: orders, shirts: shirts}
 end
 
-post '/orders/:id' do
-    id =  params[:id]
-    qty = params[:qty]
-    puts qty
+# post '/orders/:id' do
+#     id =  params[:id]
+#     qty = params[:qty]
+#     puts qty
 
-    shirt = Shirt.find(id)
+#     shirt = Shirt.find(id)
 
-    new_instock = shirt.instock - qty
-    Shirt.update({instock: new_instock})
-    redirect("/shirts")
-end
+#     new_instock = shirt.instock - qty
+#     Shirt.update({instock: new_instock})
+#     redirect("/shirts")
+# end
 
 get '/admin' do
     shirts = Shirt.all
@@ -75,6 +74,14 @@ get '/shirts/:id/edit' do
 end
 
 post '/orders' do
+    id = params[:shirt_id].to_i
+    qty = params[:qty].to_i
+
+    shirt = Shirt.find(id)
+
+    new_instock = shirt.instock.to_i - qty
+    shirt.update({instock: new_instock})
+
     order = Order.create({email: params[:email], shirt_id: params[:shirt_id], quantity: params[:qty]});
     redirect "/receipt/#{order.id}"
 end
