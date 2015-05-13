0382c82c776c563ce5d4529c9ab55c453f382a4d
JAH <kwasijames@OtuJAH.local>
2015-05-13 12:39:10 -0400

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,44 @@ Shirt.create({
 	shirt_image: "http://cdna.lystit.com/photos/2013/11/13/sunspel-white-q14-cellular-crew-neck-t-shirt-product-1-14847137-199281338.jpeg"
 
 })
+
+Shirt.create({
+	style: "Crew Neck",
+	color: "Orange",
+	instock: "5",
+	price: "9.99",
+	shirt_image: "http://hnhapparel.com/Crew-neck%20t-shirts/orange.jpg"
+})
+
+
+Shirt.create({
+	style: "V Neck",
+	color: "Black",
+	instock: "9",
+	price: "20.99",
+	shirt_image: "http://cdn.skyje.com/wp-content/uploads/2012/12/v_neck_shirt-600x600.jpg"
+})
+
+Shirt.create({
+	style: "Crew Neck",
+	color: "Orange",
+	instock: "5",
+	price: "20.99",
+	shirt_image: "http://fighterxfashion.com/wp-content/uploads/2012/04/jaco-logo-crew-shirt-grey.jpg"
+})
+
+Shirt.create({
+	style: "Crew Neck",
+	color: "Black",
+	instock: "5",
+	price: "2.99",
+	shirt_image: "http://phspanthers.com/images/cct/tail_black_tshirt.jpeg"
+})
+
+Shirt.create({
+	style: "V Neck",
+	color: "Green",
+	instock: "9",
+	price: "1.99",
+	shirt_image: "http://t-shirts.classb.com/image/47512.495.shirt.Front.jpg?1233922899"
+})
